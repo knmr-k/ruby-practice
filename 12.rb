15d54059cbdfc841137184bc915c7efa4567638a
knmr-k <kanamr.kz@gmail.com>
2023-08-22 10:32:03 +0900

    インスタンスの生成

---
+++ 12.rb
@@ -0,0 +1,33 @@
+# インスタンスの生成
+
+# クラスFruitを以下の仕様で定義してください。
+
+# 実行結果は以下のようになります。
+# 採れたて新鮮な果実です
+# リンゴは120円です
+# オレンジは200円です
+# イチゴは60円です
+
+class Fruit
+  def self.fresh
+    puts "採れたて新鮮な果実です"
+  end
+ 
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+ 
+  def introduce
+    puts "#{@name}は#{@price}円です"
+  end
+end
+ 
+ apple = Fruit.new("リンゴ", 120)
+ orange = Fruit.new("オレンジ", 200)
+ strawberry = Fruit.new("イチゴ", 60)
+ 
+ Fruit.fresh
+ apple.introduce
+ orange.introduce
+ strawberry.introduce
